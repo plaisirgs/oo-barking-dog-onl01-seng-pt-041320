260ffe4cd2a14f6589d1f248f05462a77ee2a700
GINELLE PLAISIR <plaisirginelle@gmail.com>
2020-04-29 06:35:35 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+class Dog
+  def name=(new_dog)
+    @this_name = new_dog
+  end
+  
+  def name
+    @this_name
+  end
+  
+  def bark
+    puts "woof!"
+  end
+end
+
+
+
+
