58a25d13530127049fb599b2a81747cb59b55c83
GINELLE PLAISIR <plaisirginelle@gmail.com>
2020-04-29 06:34:11 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,32 +1,16 @@
 class Dog
   def name=(new_dog)
-    this_name = new_dog
+    @this_name = new_dog
   end
   
   def name
-    this_name
+    @this_name
   end
   
   def bark
-    
+  end
 end
 
 
 
 
-# class Dog
- 
-#   def name=(dogs_name)
-#     @this_dogs_name = dogs_name
-#   end
- 
-#   def name
-#     @this_dogs_name
-#   end
-# end
- 
-# lassie = Dog.new
-# lassie.name = "Lassie"
- 
-# puts lassie.name
- 
